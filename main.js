63eb159ef2fc186775ee460c1de220479d7ecaae
Austin Carey <austinpatrickcarey@gmail.com>
2017-07-11 16:02:09 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,129 @@
+// The Form Data
+let formData = [
+  // For demonstration purposes,
+  // the first form element has been added to the HTML file as a comment
+  // compare the input in the HTML file with the contents of this first object
+  {
+    "type": "text",
+    "label": "First Name",
+    "id": "user-first-name",
+    "icon": "fa-user",
+    "options": []
+  },
+  {
+    "type": "text",
+    "label": "Last Name",
+    "id": "user-last-name",
+    "icon": "fa-user",
+    "options": []
+  },
+  {
+    "type": "email",
+    "label": "Email Address",
+    "id": "user-email",
+    "icon": "fa-envelope",
+    "options": []
+  },
+  {
+    "type": "text",
+    "label": "Current Website URL",
+    "id": "user-website",
+    "icon": "fa-globe",
+    "options": []
+  },
+  {
+    "type": "select",
+    "label": "Select Language",
+    "id": "user-language",
+    "icon": "",
+    "options": [
+      {
+        "label": "English",
+        "value": "EN"
+      },
+      {
+        "label": "French",
+        "value": "FR"
+      },
+      {
+        "label": "Spanish",
+        "value": "SP"
+      },
+      {
+        "label": "Chinese",
+        "value": "CH"
+      },
+      {
+        "label": "Japanese",
+        "value": "JP"
+      }
+    ]
+  },
+  {
+    "type": "textarea",
+    "label": "Your Comment",
+    "id": "user-comment",
+    "icon": "fa-comments",
+    "options": []
+  },
+  {
+    "type": "tel",
+    "label": "Mobile Number",
+    "id": "user-mobile",
+    "icon": "fa-mobile-phone",
+    "options": []
+  },
+  {
+    "type": "tel",
+    "label": "Home Number",
+    "id": "user-phone",
+    "icon": "fa-phone",
+    "options": []
+  }
+];
+
+// HINTS:
+// As you can see, we access the first element in the array
+// with [0] and then grab the property "label" using the "." operator
+( function(){
+  // Select the first element from the array
+  // let first = formData[ 0 ];
+
+} )();
+
+
+// -------- Your Code Goes Below this Line --------
+//
+let fields = document.querySelector('#fields');
+
+for (let i = 0; i < formData.length; i++ ){
+  let type = document.createElement('input');
+  let label = document.createElement('input');
+  let id = document.createElement('input');
+  // let icon = document.createElement('fa-awesome');
+  let options = document.createElement('input')
+
+  type.setAttribute('type', formData[i].type);
+
+  fields.appendChild( type );
+
+  console.log(type);
+}
+
+
+
+
+
+
+
+
+  // let second = formData[1];
+  // let third = formData[2];
+  // let fourth = formData[3];
+  // let fifth = formData[4];
+  // let sixth = formData[5];
+  // let seventh = formData[6];
+  // let eighth = formData[7];
+  //
+  // let inputText = document.createElement('input');
+  //   inputText.setAttribute('type', 'text');
